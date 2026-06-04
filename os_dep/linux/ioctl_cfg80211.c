bc24d5bfc28b24a5bf91768ed9b6a26e97b500b4
Luka Gejak <luka.gejak@linux.dev>
2026-06-04 15:08:33 +0000

    Call rtw_regd_init() from rtw_wiphy_register()

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -6781,7 +6781,7 @@ void rtw_cfg80211_init_wiphy(_adapter *padapter)
 	}
 #endif
 	/* init regulary domain */
-	rtw_regd_init(padapter);
+	//rtw_regd_init(padapter);
 
 	/* copy mac_addr to wiphy */
 	_rtw_memcpy(wiphy->perm_addr, adapter_mac_addr(padapter), ETH_ALEN);
@@ -7265,6 +7265,8 @@ int rtw_wiphy_register(struct wiphy *wiphy)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) || defined(RTW_VENDOR_EXT_SUPPORT)
 	rtw_cfgvendor_attach(wiphy);
 #endif
+
+	rtw_regd_init(wiphy);
 
 	return wiphy_register(wiphy);
 }
